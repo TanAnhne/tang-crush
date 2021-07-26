5e86774d596d16b4116bc6fef0622054e0dd88f5
TanAnhne <87972770+TanAnhne@users.noreply.github.com>
2021-07-26 16:23:10 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Babe à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introTitle: 'Sophia èeee!',
+    introDesc: `Chúc mừng sinh nhật ai zảy,làm picsart khó lắm đó`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    title: 'âu kaay anh đã trằn trọc suốt đêm qua để đặt ra một câu hỏi là 😙',
+    desc: 'Tại sao tên em không phải là mẫn nhi ',
+    btnYes: 'Ai biết :))) ',
+    btnNo: 'Khùng hả TẤN ANH :))) ',
+    question:'Trên thế giới này có hơn 7 tỉ nụ cười,nhưng mà tại sao nụ cười của em mới là đẹp nhất ? ',
+    btnReply: 'Gửi cho aiu Tấn Anh đẹp trai <3',
+    reply: 'khum biết nữa nòoo ' ,
+    mess: 'Vì yêu đó cục cưng :v lêu lêu 🥰 ô kìa mắc cỡ chưa 😘😘',
+    messDesc: 'Code by Tan Anh huheo :3 ',
+    btnAccept: 'giỏi quá đêyyyyyy ',
     messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
